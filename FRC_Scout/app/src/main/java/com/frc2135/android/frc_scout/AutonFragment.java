2c5890b1f2c0568bd2771ad05a4a2400dd37f404
Jeff Mullins <jmullins16@users.noreply.github.com>
2021-05-02 22:47:41 -0700

    Renamed project and updated company domain

---
+++ FRC_Scout/app/src/main/java/com/frc2135/android/frc_scout/AutonFragment.java
@@ -0,0 +1,125 @@
+package com.frc2135.android.frc_scout;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+
+public class AutonFragment extends Fragment {
+    private static final String TAG = "AutonFragment";
+
+    private TextView mLowPoints;
+    private TextView mHighPoints;
+
+    private Button mLowPortPointsDec;
+    private Button mLowPortPointsInc;
+    private Button mHighPortPointsDec;
+    private Button mHighPortPointsInc;
+
+    private EditText mTeamNumberField;
+    private EditText mMatchNumberField;
+
+    private CheckBox mCheckBox;
+
+    private MatchData mMatchData;
+
+    private ActionBar t;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+
+        mMatchData = ((ScoutingActivity)getActivity()).getCurrentMatch();
+        Log.d(TAG, mMatchData.getMatchID());
+
+        t =  ((AppCompatActivity)getActivity()).getSupportActionBar();
+        t.setTitle("Autonomous: ");
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+        //Creates a view using the specific fragment layout, match_data_fragment
+        View v = inflater.inflate(R.layout.auton_fragment, parent, false);
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+
+        //Sets up TextView that displays low points, setting 0 as the default
+        mLowPoints = (TextView)v.findViewById(R.id.lowportpoints);
+        mLowPoints.setText(0 + "");
+
+        //Sets up TextView that displays high points, setting 0 as the default
+        mHighPoints = (TextView)v.findViewById(R.id.highportpoints);
+        mHighPoints.setText(0+ "");
+
+
+        //Connects the decrement button for low points and sets up a listener that detects when the button is clicked
+        mLowPortPointsDec = (Button)v.findViewById(R.id.lowportpointsdec);
+        mLowPortPointsDec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Decreases displayed point value by 1
+                mLowPoints.setText((Integer.parseInt(mLowPoints.getText().toString())- 1) + "");
+            }
+        });
+
+        //Connects the increment button for low points and sets up a listener that detects when the button is clicked
+        mLowPortPointsInc = (Button)v.findViewById(R.id.lowportpointsinc);
+        mLowPortPointsInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Increases displayed point value by 1
+                mLowPoints.setText((Integer.parseInt(mLowPoints.getText().toString())+ 1) + "");
+            }
+        });
+
+        //Connects the decrement button for high points and sets up a listener that detects when the button is clicked
+        mHighPortPointsDec = (Button)v.findViewById(R.id.highportpointsdec);
+        mHighPortPointsDec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Decreases displayed point value by 1
+                mHighPoints.setText((Integer.parseInt(mHighPoints.getText().toString())- 1) + "");
+            }
+        });
+
+        //Connects the increment button for high points and sets up a listener that detects when the button is clicked
+        mHighPortPointsInc = (Button)v.findViewById(R.id.highportpointsinc);
+        mHighPortPointsInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Increases displayed point value by 1
+                mHighPoints.setText((Integer.parseInt(mHighPoints.getText().toString())+ 1) + "");
+            }
+        });
+
+        //Connects the checkbox for passing the initiation line and sets up a listener to detect when the checked status is changed
+        mCheckBox = (CheckBox)v.findViewById(R.id.auto_line_checkbox);
+        mCheckBox.setChecked(mMatchData.getPassedInitLine());
+
+
+
+        mHighPoints.setText(mMatchData.getAutonHighPoints()+"");
+        mLowPoints.setText(mMatchData.getAutonLowPoints()+"");
+
+        return v;
+    }
+
+    public void updateAutonData(){
+        mMatchData.setAutonLowPoints(Integer.parseInt(mLowPoints.getText().toString()));
+        mMatchData.setAutonOuterPoints(Integer.parseInt(mHighPoints.getText().toString()));
+        mMatchData.setPassedInitLine(mCheckBox.isChecked());
+    }
+}
---
+++ FRC_Scout/app/src/main/java/com/frc2135/android/frc_scout/QRFragment.java
@@ -0,0 +1,96 @@
+package com.frc2135.android.frc_scout;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.icu.text.SimpleDateFormat;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.fragment.app.DialogFragment;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+
+import java.util.Date;
+
+import zxing.Contents;
+import zxing.QRCodeEncoder;
+
+
+
+public class QRFragment extends DialogFragment {
+    private static String TAG = "QRFragment";
+    private ImageView mImageView;
+    private Button mBacktoMenuButton;
+    private String heading;
+    private String message;
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        View v = getActivity().getLayoutInflater().inflate(R.layout.qr_fragment, null);
+
+        heading =  getArguments().getString("dialog heading");
+        message = getArguments().getString("stats");
+
+
+        ImageView mImageView = (ImageView)v.findViewById(R.id.matchdata_qr);
+
+        int qrCodeDimension = 1000;
+
+        Log.d(TAG, message);
+
+        QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(message, null,
+                Contents.Type.TEXT, BarcodeFormat.QR_CODE.toString(), qrCodeDimension);
+
+        try {
+            Bitmap bitmap = qrCodeEncoder.encodeAsBitmap();
+            mImageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            Log.d("QRFragment", e+"");
+        }
+
+
+        return new AlertDialog.Builder(getActivity()).setView(v).setTitle(heading).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                //sendResult(Activity.RESULT_OK);
+            }
+        }).create();
+    }
+
+
+    public static QRFragment newInstance(MatchData mMatchData){
+
+        QRFragment fragment = new QRFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("dialog heading", mMatchData.getCompetition()+"-"+mMatchData.getTeamNumber()+"-"+mMatchData.getMatchNumber()+ "-" + formattedDate(mMatchData.getTimestamp()));
+        bundle.putString("stats", mMatchData.encodeToTSV());
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
+    public static String formattedDate(Date d){
+        SimpleDateFormat dt = new SimpleDateFormat("E MMM dd hh:mm:ss z yyyy");
+        Date date = null;
+        try{
+            date=dt.parse(d.toString());
+        }catch(Exception e){
+            Log.d("SignInFragment", e.getMessage());
+        }
+        SimpleDateFormat dt1 = new SimpleDateFormat("hh:mm:ss");
+
+        if(date == null) {
+            return null;
+        }
+        else { return (dt1.format(date)); }
+    }
+
+
+}
