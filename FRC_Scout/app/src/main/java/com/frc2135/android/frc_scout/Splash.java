4d010db18adf5708942c237452d607a8948c95ef
Niti Reddy <nreddy.24@presentationhs.org>
2022-01-30 11:23:38 -0800

    Increased the display length of the splash screen

--- FRC_Scout/app/src/main/java/com/frc2135/android/frc_scout/Splash.java
+++ FRC_Scout/app/src/main/java/com/frc2135/android/frc_scout/Splash.java
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Splash extends AppCompatActivity {
 
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 1475;
 
     /** Called when the activity is first created. */
     @Override
