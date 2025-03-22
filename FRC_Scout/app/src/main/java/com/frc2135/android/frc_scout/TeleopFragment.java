5683ff4ba43d6de9fa80ebe68f18b72b474d0b76
CCameron <ccameron.27@presentationhs.org>
2025-03-22 13:36:08 -0700

    removed all checkboxes and defense radio buttons from teleop

--- FRC_Scout/app/src/main/java/com/frc2135/android/frc_scout/TeleopFragment.java
+++ FRC_Scout/app/src/main/java/com/frc2135/android/frc_scout/TeleopFragment.java
@@ -9,15 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+
 
 
 /** @noinspection SpellCheckingInspection*/
@@ -27,25 +25,13 @@ public class TeleopFragment extends Fragment
     private static final int MAX_NUM_ALGAE = 7;     // algae max expected high total
     private static final int MAX_NUM_CORAL= 11;     // coral max expected high total 
 
-    private CheckBox m_pickUpCoralCkbx;
-    private CheckBox m_pickUpAlgaeCkbx;
-    private CheckBox m_knockAlgaeCkbx;
-    private CheckBox m_algaeFromReefCkbx;
-    private CheckBox m_holdBothCkbx;
-
     private TextView m_coralAcquiredTotal;
     private Button   m_coralAcquiredDecrButton;
     private Button   m_coralAcquiredIncrButton;
     private TextView m_algaeAcquiredTotal;
     private Button   m_algaeAcquiredDecrButton;
     private Button   m_algaeAcquiredIncrButton;
-
-    private RadioGroup m_defenseButtonGroup;
-    private RadioButton m_defenseNone;
-    private RadioButton m_defenseLow;
-    private RadioButton m_defenseMedium;
-    private RadioButton m_defenseHigh;
-
+    
     private TextView m_teleopL1Total;
     private TextView m_teleopL2Total;
     private TextView m_teleopL3Total;
@@ -310,41 +296,6 @@ public class TeleopFragment extends Fragment
             }
         });
 
-        m_pickUpCoralCkbx = v.findViewById(R.id.pickup_coral);
-        m_pickUpCoralCkbx.setChecked(m_matchData.getPickUpCoral());
-
-        m_pickUpAlgaeCkbx = v.findViewById(R.id.pickup_algae);
-        m_pickUpAlgaeCkbx.setChecked(m_matchData.getPickUpAlgae());
-
-        m_knockAlgaeCkbx = v.findViewById(R.id.knock_algae_off);
-        m_knockAlgaeCkbx.setChecked(m_matchData.getKnockOffAlgae());
-
-        m_algaeFromReefCkbx = v.findViewById(R.id.algae_from_reef);
-        m_algaeFromReefCkbx.setChecked(m_matchData.getAlgaeFromReef());
-
-        m_holdBothCkbx = v.findViewById(R.id.hold_both_elements);
-        m_holdBothCkbx.setChecked(m_matchData.getHoldBothElements());
-
-        m_defenseButtonGroup = v.findViewById(R.id.defense_buttons);
-        m_defenseNone = v.findViewById(R.id.defense_none);
-        m_defenseLow = v.findViewById(R.id.defense_low);
-        m_defenseMedium = v.findViewById(R.id.defense_medium);
-        m_defenseHigh = v.findViewById(R.id.defense_high);
-        m_defenseNone.setChecked(false);
-        m_defenseLow.setChecked(false);
-        m_defenseMedium.setChecked(false);
-        m_defenseHigh.setChecked(false);
-
-        int defValue = m_matchData.getPlayedDefense();
-        if (defValue == 0)
-            m_defenseNone.setChecked(true);
-        else if(defValue == 1)
-          m_defenseLow.setChecked(true);
-        else if(defValue == 2)
-            m_defenseMedium.setChecked(true);
-        else if(defValue == 3)
-            m_defenseHigh.setChecked(true);
-
         // Check acquired totals for MAX
         if (isGreaterThanMax(m_coralAcquiredTotal,true))
             m_coralAcquiredTotal.setTextColor(Color.RED);
@@ -363,29 +314,6 @@ public class TeleopFragment extends Fragment
 
         return v;
     }
-    
-    public int getCurrentDefenseLevel()
-    {
-        // Returns the integer climb level that is current checked in the radio buttons
-        int rtn = 0;
-        if (m_defenseButtonGroup.getCheckedRadioButtonId() == m_defenseNone.getId())
-        {
-            rtn = 0;
-        }
-        if (m_defenseButtonGroup.getCheckedRadioButtonId() == m_defenseLow.getId())
-        {
-            rtn = 1;
-        }
-        if (m_defenseButtonGroup.getCheckedRadioButtonId() == m_defenseMedium.getId())
-        {
-            rtn = 2;
-        }
-        if (m_defenseButtonGroup.getCheckedRadioButtonId() == m_defenseHigh .getId())
-        {
-            rtn = 3;
-        }
-        return rtn;
-    }
 
     public void updateTeleopData()
     {
@@ -400,12 +328,5 @@ public class TeleopFragment extends Fragment
         m_matchData.setTeleopCoralL3(Integer.parseInt(m_teleopL3Total.getText().toString()));
         m_matchData.setTeleopCoralL4(Integer.parseInt(m_teleopL4Total.getText().toString()));
 
-        m_matchData.setPickUpCoral(m_pickUpCoralCkbx.isChecked());
-        m_matchData.setPickUpAlgae(m_pickUpAlgaeCkbx.isChecked());
-        m_matchData.setKnockOffAlgae(m_knockAlgaeCkbx.isChecked());
-        m_matchData.setAlgaeFromReef(m_algaeFromReefCkbx.isChecked());
-        m_matchData.setHoldBothElements(m_holdBothCkbx.isChecked());
-
-        m_matchData.setPlayedDefense(getCurrentDefenseLevel());
     }
 }
